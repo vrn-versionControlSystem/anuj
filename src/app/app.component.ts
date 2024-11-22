89452ca4710b2d66b122211835201d82e81d3a39
Vasim Ramjan Nadaf <vrnadaf@gmail.com>
2024-11-22 17:04:47 +0530

    changes in app.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, HomeComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
